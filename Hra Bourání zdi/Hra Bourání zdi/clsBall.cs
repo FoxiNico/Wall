8f2efcb922f9880893d3716975d99515bfe3f08e
Nico <133045294+FoxiNico@users.noreply.github.com>
2023-05-09 20:14:01 +0200

    Add files via upload

---
+++ Hra Bourání zdi/Hra Bourání zdi/clsBall.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hra_Bourání_zdi
+{
+    class clsBall
+    {
+        //grafika na kreslení
+        Graphics mobjGrafika;
+
+        //kulička 
+        int mintXBall, mintYBall;
+        int mintPohybX, mintPohybY;
+        const int mintRBall = 15;
+        const int mintRychlostPosunu = 9;
+
+        //-----------------------------------------------
+        //konstruktor
+        //-----------------------------------------------
+
+        public clsBall(Graphics pobjPlatno)
+        {
+            mobjGrafika = pobjPlatno;
+
+            mintXBall = (int)pobjPlatno.VisibleClipBounds.Width / 2;
+            mintYBall = (int)pobjPlatno.VisibleClipBounds.Height / 2;
+            mintPohybX = mintRychlostPosunu;
+            mintPohybY = mintRychlostPosunu;
+        }
+
+        // načtení hodnot souřadnic kuličky
+        public int intXBall
+        {
+            get
+            { return mintXBall; }
+        }
+
+        public int intYBall
+        {
+            get
+            { return mintXBall; }
+        }
+
+        public int intWBall
+        {
+            get
+            { return mintXBall; }
+        }
+
+        public int intHBall
+        {
+            get
+            { return mintXBall; }
+        }
+
+        public void Pohyb()
+        {
+            //vykreslení kuličky
+            mobjGrafika.FillEllipse(Brushes.RosyBrown, mintXBall, mintYBall, mintRBall, mintRBall);
+
+            //posun
+            mintXBall += mintPohybX;
+            mintYBall += mintPohybY;
+
+            //test kolize na hranách
+            if (mintYBall + mintRBall > (int)mobjGrafika.VisibleClipBounds.Height)
+                mintPohybY = mintPohybY * (-1);
+
+            if (mintYBall < 0)
+                mintPohybY = mintPohybY * (-1);
+
+            if (mintXBall + mintRBall > (int)mobjGrafika.VisibleClipBounds.Width)
+                mintPohybX = mintPohybX * (-1);
+
+            if (mintXBall < 0)
+                mintPohybX = mintPohybX * (-1);
+        }
+    }
+}
---
+++ Hra Bourání zdi/Hra Bourání zdi/clsBrick.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hra_Bourání_zdi
+{
+    class clsBrick
+    {
+        Graphics mobjGrafika;
+
+        //cihla
+        int mintXBrick, mintYBrick;
+        int mintVyskaBrick, mintSirkaBrick;
+        bool mblViditelnost;
+
+        //-----------------------------------------------
+        //konstruktor
+        //-----------------------------------------------
+
+        public clsBrick(Graphics objPlatno, int intXBrick, int intYBrick, int intSirkaBrick, int intVyskaBrick)
+        {
+            mobjGrafika = objPlatno;
+            mintXBrick = intXBrick;
+            mintYBrick = intYBrick;
+            mintVyskaBrick = intVyskaBrick;
+            mintSirkaBrick = intSirkaBrick;
+            mblViditelnost = true;
+        }
+        
+        //-----------------------------------------------
+        //vykreslení cihly
+        // - vrací true při kolizi
+        //-----------------------------------------------
+
+        public void Nakrelit()
+        {
+            //test viditelnosti
+            if (mblViditelnost == false) return;
+
+            //vykreslení
+            mobjGrafika.FillRectangle(Brushes.Orange, mintXBrick, mintYBrick, mintSirkaBrick, mintVyskaBrick);
+        }
+
+        //-----------------------------------------------
+        //test kolize cihly a kuličky
+        //-----------------------------------------------
+
+        public bool TestKolize(int intXBall, int intYBall, int intWBall, int intHBall)
+        {
+            //test viditelnosti cihly
+            if (mblViditelnost == false) return false;
+
+            // test kolize
+            if (intYBall <= mintYBrick)
+            {
+                //cihla není vidět
+                mblViditelnost = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
